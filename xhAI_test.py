f7e278bc28598dbcdecace197b52b9ac0eab03f6
DraekoTage <164750166+DraekoTage@users.noreply.github.com>
2024-11-07 11:46:49 +0800

    Add files via upload

--- xhAI_test.py
+++ xhAI_test.py
@@ -6,11 +6,11 @@ from pynput.keyboard import Listener, Key, KeyCode
 
 
 #星火认知大模型Spark Max的URL值，其他版本大模型URL值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
-SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
+SPARKAI_URL = ''
 #星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
-SPARKAI_APP_ID = '316479e5'
-SPARKAI_API_SECRET = 'MjQyMjk4OWI2YTU2NzE4OTlkOWEwZmZi'
-SPARKAI_API_KEY = '3f1831116ec5ed3d3e0b2f1b547ae3ee'
+SPARKAI_APP_ID = ''
+SPARKAI_API_SECRET = ''
+SPARKAI_API_KEY = ''
 #星火认知大模型Spark Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
 SPARKAI_DOMAIN = 'generalv3.5'
 a = "自我介绍一下"
